64e05a317c2d97343b0e2a8818b80270915ce8b6
mperrakis2 <169139627+mperrakis2@users.noreply.github.com>
2024-09-22 19:11:04 +0300

    fixed bug in script that copied shim signed binaries to ESP

--- usr/local/bin/cpshim.sh
+++ usr/local/bin/cpshim.sh
@@ -2,8 +2,11 @@
 
 # copy shim binaries to ESP
 
-bootdir="$(bootctl -p)"
-cp /usr/share/shim-signed/shimx64.efi "$bootdir"/EFI/BOOT/BOOTX64.EFI
-cp /usr/share/shim-signed/mmx64.efi "$bootdir"/EFI/BOOT/
-cp /usr/share/shim-signed/shimx64.efi "$bootdir"/EFI/"$(uname -n)"/
-cp /usr/share/shim-signed/mmx64.efi "$bootdir"/"$(uname -n)"/
+bl_dir="$(bootctl -p)/EFI/"
+def_bl_dir="$bl_dir/BOOT/"
+distro_bl_dir="$bl_dir/$(uname -n)/"
+
+cp /usr/share/shim-signed/shimx64.efi "$def_bl_dir"/BOOTX64.EFI
+cp /usr/share/shim-signed/mmx64.efi "$def_bl_dir"
+cp /usr/share/shim-signed/shimx64.efi "$distro_bl_dir"
+cp /usr/share/shim-signed/mmx64.efi "$distro_bl_dir"
