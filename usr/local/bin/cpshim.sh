89b11fcc81c3649706ab5d61afd4e0953e1bfbc3
mperrakis2 <169139627+mperrakis2@users.noreply.github.com>
2024-09-23 12:47:53 +0300

    * refactored cpshim.sh to be more generic * copy grub bootloader to EFI/BOOt only if it exists (grub_sbsign.sh) * install and sign grub bootloaders only if ESP exists (grub_sbsign.sh)

--- usr/local/bin/cpshim.sh
+++ usr/local/bin/cpshim.sh
@@ -5,8 +5,10 @@
 bl_dir="$(bootctl -p)/EFI/"
 def_bl_dir="$bl_dir/BOOT/"
 distro_bl_dir="$bl_dir/$(uname -n)/"
+shim_dir=/usr/share/shim-signed/
+shim_fname=shimx64.efi
+mm_fname=mmx64.efi
 
-cp /usr/share/shim-signed/shimx64.efi "$def_bl_dir"/BOOTX64.EFI
-cp /usr/share/shim-signed/mmx64.efi "$def_bl_dir"
-cp /usr/share/shim-signed/shimx64.efi "$distro_bl_dir"
-cp /usr/share/shim-signed/mmx64.efi "$distro_bl_dir"
+cp "$shim_dir/$shim_fname" "$def_bl_dir"/BOOTX64.EFI
+cp "$shim_dir/$mm_fname" "$def_bl_dir"
+cp "$shim_dir/$shim_fname" "$shim_dir/$mm_fname" "$distro_bl_dir"
--- usr/local/bin/grub_sbsign.sh
+++ usr/local/bin/grub_sbsign.sh
@@ -38,41 +38,33 @@ exit_on_error $? "$ptn"
 ptn_data=($(lsblk --noheadings --output PKNAME,PARTN "$ptn" | xargs))
 exit_on_error $? "${ptn_data[*]}"
 
-# get distro string
-distro=$(uname -n)
-exit_on_error $? "$res"
+# check if ESP exists
+parted /dev/"${ptn_data[0]}" print | grep -q esp
+declare -i is_esp=$(( ! $? ))
 
-if efibootmgr > /dev/null; then
+if (( is_esp )); then
     # modules to embed in grub bootloader 
     grub_modules="all_video boot btrfs cat chain configfile echo efifwsetup efinet "\
-    "ext2 fat font gettext gfxmenu gfxterm gfxterm_background gzio "\
-    "halt help hfsplus iso9660 jpeg keystatus loadenv loopback linux "\
-    "ls lsefi lsefimmap lsefisystab lssal memdisk minicmd normal ntfs "\
-    "part_apple part_msdos part_gpt password_pbkdf2 png probe reboot "\
-    "regexp search search_fs_uuid search_fs_file search_label sleep "\
-    "smbios squash4 test true video xfs zfs zfscrypt zfsinfo cpuid "\
-    "play tpm cryptodisk gcry_arcfour gcry_blowfish gcry_camellia "\
-    "gcry_cast5 gcry_crc gcry_des gcry_dsa gcry_idea gcry_md4 gcry_md5 "\
-    "gcry_rfc2268 gcry_rijndael gcry_rmd160 gcry_rsa gcry_seed "\
-    "gcry_serpent gcry_sha1 gcry_sha256 gcry_sha512 gcry_tiger "\
-    "gcry_twofish gcry_whirlpool luks lvm mdraid09 mdraid1x raid5rec "\
-    "raid6rec http tftp"
+"ext2 fat font gettext gfxmenu gfxterm gfxterm_background gzio "\
+"halt help hfsplus iso9660 jpeg keystatus loadenv loopback linux "\
+"ls lsefi lsefimmap lsefisystab lssal memdisk minicmd normal ntfs "\
+"part_apple part_msdos part_gpt password_pbkdf2 png probe reboot "\
+"regexp search search_fs_uuid search_fs_file search_label sleep "\
+"smbios squash4 test true video xfs zfs zfscrypt zfsinfo cpuid "\
+"play tpm cryptodisk gcry_arcfour gcry_blowfish gcry_camellia "\
+"gcry_cast5 gcry_crc gcry_des gcry_dsa gcry_idea gcry_md4 gcry_md5 "\
+"gcry_rfc2268 gcry_rijndael gcry_rmd160 gcry_rsa gcry_seed "\
+"gcry_serpent gcry_sha1 gcry_sha256 gcry_sha512 gcry_tiger "\
+"gcry_twofish gcry_whirlpool luks lvm mdraid09 mdraid1x raid5rec "\
+"raid6rec http tftp"
 
-    # install grub to ESP
+    # install grub to ESP/<distro>
     res=$(grub-install --modules="$grub_modules" --sbat=/usr/share/grub/sbat.csv \
                        --efi-directory="$bootdir" --recheck --target=x86_64-efi)
     exit_on_error $? "$res"
-
-    # copy grub bootloader to dir used by external devices to boot, i.e. EFI/BOOT
-    # bl -> bootloader
-    bl_dir="$bootdir/EFI/"
-    def_bl_dir="$bl_dir/BOOT/"
-    bl_name="grubx64.efi"
-    full_bl_path="$bl_dir/$distro/$bl_name"
-    res=$(cp "$full_bl_path" "$def_bl_dir")
-    exit_on_error $? "$res"
 fi
 
+# check if bios partition exists
 if parted /dev/"${ptn_data[0]}" print | grep -q bios_grub; then
     # install grub to bios partition
     res=$(grub-install --boot-directory='$dstdir' --recheck --target=i386-pc \
@@ -80,27 +72,46 @@ if parted /dev/"${ptn_data[0]}" print | grep -q bios_grub; then
     exit_on_error $? "$res"
 fi
 
-res=$(mokutil --sb-state) # get secure boot state
+# get distro string
+distro=$(uname -n)
+exit_on_error $? "$res"
 
-if [[ "${res,,}" == "secureboot enabled" ]]; then
+# install efi boot entry even if not secure boot
+if efibootmgr > /dev/null; then
     # create new UEFI boot entry for shim
     res=$(efibootmgr --unicode --disk /dev/"${ptn_data[0]}" --part "${ptn_data[1]}" \
                      --create --label "Shim" --loader /EFI/"$distro"/shimx64.efi)
     exit_on_error $? "$res"
+fi
 
-    # sign grub bootloader used by internal device to boot, i.e. EFI/<distro>
+# if ESP exists, sign grub bootloaders
+if (( is_esp )); then
+    bl_dir="$bootdir/EFI/" # bl -> bootloader
+    bl_name="grubx64.efi"
     keyfile="MOK.key"
     crtfile="MOK.crt"
+
     cd "$bl_dir"
-    if ! sbverify --cert "$crtfile" "$full_bl_path" &>/dev/null; then
-        res=$(sbsign --key "$keyfile" --cert "$crtfile" --output "$full_bl_path" \
-                                                                 "$full_bl_path")
+
+    def_bl_dir="$bl_dir/BOOT/"
+    if [[ -s "$def_bl_dir" ]]; then
+        # copy grub bootloader to EFI/BOOT
+        full_bl_path="$bl_dir/$distro/$bl_name"
+        res=$(cp "$full_bl_path" "$def_bl_dir")
         exit_on_error $? "$res"
+
+        # sign grub bootloader in EFI/BOOT used by external device to boot
+        full_bl_path="$def_bl_dir/$bl_name"
+        if ! sbverify --cert "$crtfile" "$full_bl_path" &>/dev/null; then
+            res=$(sbsign --key "$keyfile" --cert "$crtfile" --output "$full_bl_path" \
+                                                                     "$full_bl_path")
+            exit_on_error $? "$res"
+        fi
     fi
 
-    # sign grub bootloader used by external device to boot, i.e. EFI/BOOT
-    full_bl_path="$def_bl_dir/$bl_name"
-    if ! sbverify --cert "$crtfile" "$bootloader" &>/dev/null; then
+    # sign grub bootloader in EFI/<distro> used by internal device to boot
+    full_bl_path="$bl_dir/$distro/$bl_name"
+    if ! sbverify --cert "$crtfile" "$full_bl_path" &>/dev/null; then
         res=$(sbsign --key "$keyfile" --cert "$crtfile" --output "$full_bl_path" \
                                                                  "$full_bl_path")
         exit_on_error $? "$res"
