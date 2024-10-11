01e039e3edd6742e4d41500a10fee8263da6522d
mperrakis2 <169139627+mperrakis2@users.noreply.github.com>
2024-10-11 12:45:22 +0300

    grub-sbsign.sh & bootable_usb.txt --------------------------------- updated instructions to generate correctly default and distro based bootloaders

--- usr/local/bin/grub_sbsign.sh
+++ usr/local/bin/grub_sbsign.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/bash
 
-# install and sign grub bootloader, create UEFI boot entry and update grub cfg file
+# install and sign grub bootloader and update grub cfg file
+
+shopt -s extglob
 
 # if first param is non-zero, display error message and exit with first param
 # $1    : int, the result returned from a command
@@ -23,8 +25,7 @@ exit_on_error() {
     fi
 }
 
-# redirect stderr to stdout
-exec 2>&1
+exec 2>&1 # redirect stderr to stdout
 
 # get boot dir
 bootdir=$(bootctl -p)
@@ -34,13 +35,25 @@ exit_on_error $? "$bootdir"
 ptn=$(findmnt --noheadings --output SOURCE --target "$bootdir")
 exit_on_error $? "$ptn"
 
-# get device name & boot partition number
+# get drive name & boot partition number
 ptn_data=($(lsblk --noheadings --output PKNAME,PARTN "$ptn" | xargs))
 exit_on_error $? "${ptn_data[*]}"
+
+# check if bios partition exists
+if parted /dev/"${ptn_data[0]}" print | grep -q bios_grub; then
+    # install grub to bios partition
+    res=$(grub-install --boot-directory='$dstdir' --recheck --target=i386-pc \
+                       /dev/"${ptn_data[0]}")
+    exit_on_error $? "$res"
+fi
 
 # check if ESP exists
 parted /dev/"${ptn_data[0]}" print | grep -q esp
 declare -i is_esp=$(( ! $? ))
+
+arch=$(uname -m)                # get architecture
+arch="${arch//+([[:digit:]])_}" # remove chars not part of bl filename
+distro="$(uname -n)"
 
 if (( is_esp )); then
     # modules to embed in grub bootloader 
@@ -64,59 +77,61 @@ if (( is_esp )); then
 "time tpm tr true usb usbtest video videoinfo videotest videotest_checksum "\
 "video_bochs video_cirrus video_colors video_fb xfs xzio zfs zfscrypt zfsinfo"
 
+    # install grub to ESP/BOOT
+    res=$(grub-install --modules="$grub_modules" --sbat=/usr/share/grub/sbat.csv \
+                       --efi-directory="$bootdir" --removable --recheck \
+                       --target=x86_64-efi)
+    exit_on_error $? "$res"
+
     # install grub to ESP/<distro>
     res=$(grub-install --modules="$grub_modules" --sbat=/usr/share/grub/sbat.csv \
                        --efi-directory="$bootdir" --recheck --target=x86_64-efi)
     exit_on_error $? "$res"
-fi
 
-# check if bios partition exists
-if parted /dev/"${ptn_data[0]}" print | grep -q bios_grub; then
-    # install grub to bios partition
-    res=$(grub-install --boot-directory='$dstdir' --recheck --target=i386-pc \
-                       /dev/"${ptn_data[0]}")
-    exit_on_error $? "$res"
-fi
+    bl_dir="$bootdir/EFI/"     # bl -> bootloader
+    def_bl_dir="$bl_dir"/BOOT/ # bootloader dir for external drives
+    bl_path="$def_bl_dir"/BOOT"${arch^^}".EFI
+    bl_name="grub$arch.efi"
 
-# get distro string
-distro=$(uname -n)
-exit_on_error $? "$res"
+    mv "$bl_path" "$def_bl_dir/$bl_name" # rename bootloader
 
-# install efi boot entry even if not secure boot
-if efibootmgr > /dev/null; then
-    # create new UEFI boot entry for shim
-    res=$(efibootmgr --unicode --disk /dev/"${ptn_data[0]}" --part "${ptn_data[1]}" \
-                     --create --label "Shim" --loader /EFI/"$distro"/shimx64.efi)
-    exit_on_error $? "$res"
-fi
-
-# if ESP exists, sign grub bootloaders
-if (( is_esp )); then
-    bl_dir="$bootdir/EFI/" # bl -> bootloader
-    bl_name="grubx64.efi"
-    keyfile="MOK.key"
-    crtfile="MOK.crt"
+    # copy shim to default bootloader dir so that external drives can boot
+    cp /usr/share/shim-signed/shim"$arch".efi "$bl_path"
 
     cd "$bl_dir"
 
-    # sign grub bootloader in EFI/<distro> used by internal device to boot
-    full_bl_path="$bl_dir/$distro/$bl_name"
-    if ! sbverify --cert "$crtfile" "$full_bl_path" &>/dev/null; then
-        res=$(sbsign --key "$keyfile" --cert "$crtfile" --output "$full_bl_path" \
-                                                                 "$full_bl_path")
+    keyfile="MOK.key"
+    crtfile="MOK.crt"
+
+    # sign grub bootloader in EFI/BOOT used by external drive to boot
+    bl_path="$def_bl_dir/$bl_name"
+    if ! sbverify --cert "$crtfile" "$bl_path" &>/dev/null; then
+        res=$(sbsign --key "$keyfile" --cert "$crtfile" --output "$bl_path" \
+                                                                 "$bl_path")
         exit_on_error $? "$res"
     fi
 
-    def_bl_dir="$bl_dir/BOOT/"
-    if [[ -s "$def_bl_dir" ]]; then
-        # copy grub bootloader to EFI/BOOT
-        res=$(cp "$full_bl_path" "$def_bl_dir")
+    # sign grub bootloader in EFI/<distro> used by internal drive to boot
+    bl_path="$bl_dir/$distro/$bl_name"
+    if ! sbverify --cert "$crtfile" "$bl_path" &>/dev/null; then
+        res=$(sbsign --key "$keyfile" --cert "$crtfile" --output "$bl_path" \
+                                                                 "$bl_path")
         exit_on_error $? "$res"
     fi
+
+fi
+
+# install efi boot entry even if not secure boot
+if efibootmgr &> /dev/null; then
+    # create new UEFI boot entry for shim
+    res=$(efibootmgr --unicode --disk /dev/"${ptn_data[0]}" --part "${ptn_data[1]}" \
+                     --create --label "Shim" --loader /EFI/"$distro"/shim"$arch".efi)
+    exit_on_error $? "$res"
 fi
 
 # update grub config file
-res=$(grub-mkconfig -o "$bootdir"/grub/grub.cfg)
+grub_cfg_path="$bootdir"/grub/grub.cfg
+res=$(grub-mkconfig -o "$grub_cfg_path")
 exit_on_error $? "$res"
 
 res=$(mokutil --sb-state) # get secure boot state
@@ -125,11 +140,10 @@ res=$(mokutil --sb-state) # get secure boot state
 # bootloader sometimes displays errors like 'error: command failed.' or
 # 'error: prohibited by secure boot policy'
 [[ "${res,,}" =~ enabled ]] &&
-    sed -i -E 's|^(\s*insmod.*)$|true #\1|g' "$bootdir"/grub/grub.cfg
+    sed -i -E 's|^(\s*insmod.*)$|true #\1|g' "$grub_cfg_path"
 
 # if grub cfg file contains password hash restrict permissions
-grep -q password_pbkdf2 "$bootdir"/grub/grub.cfg &&
-    chmod o-r "$bootdir"/grub/grub.cfg
+grep -q password_pbkdf2 "$grub_cfg_path" && chmod o-r "$grub_cfg_path"
 
 exec 2> /dev/tty # restore stderr
 
