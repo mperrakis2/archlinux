9eb049cf46e5e9cb336ac9807873a08a6c411808
mperrakis2 <169139627+mperrakis2@users.noreply.github.com>
2024-10-15 22:29:06 +0300

    pkg_maint.timer & override.conf ------------------------------- change from monotonic to realtime timer
    upd_dbs_and_keyring.sh
    ----------------------
    updated code not to use pacman alias in script
    
    protonvpn.service
    -----------------
    updated to use tzupdate instead of curl and timedatectl

--- usr/local/bin/upd_dbs_and_keyring.sh
+++ usr/local/bin/upd_dbs_and_keyring.sh
@@ -4,7 +4,14 @@
 # /etc/skel/.bashrc
  
 declare -i err=0
- 
+
+pacman_alias=""
+alias pacman &> /dev/null
+if (( $? == 0 )); then # save pacman alias if any
+    pacman_alias="${BASH_ALIASES[pacman]}"
+    unalias pacman # delete pacman alias
+fi
+
 if pacman -Qu archlinux-keyring; then       # check update for keyring
     pacman --noconfirm -S archlinux-keyring # update keyring
     let err=$?
@@ -13,5 +20,8 @@ if pacman -Qu archlinux-keyring; then       # check update for keyring
         let err=$?
     fi
 fi
- 
+
+[[ "$pacman_alias" ]] && # restore pacman alias if any
+    alias pacman="$pacman_alias"
+
 exit $err
