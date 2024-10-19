56339a32fd388eceb90cf419a6557f9094bd46fc
mperrakis2 <169139627+mperrakis2@users.noreply.github.com>
2024-10-19 21:08:37 +0300

    clone.txt & scripts_and_files.txt --------------------------------- updated groups under Notes app
    override.conf
    -------------
    launch script which will update intel gfx conf file first and then
    launch lightdm
    
    lightdm.sh
    ----------
    update intel gfx conf file first and then launch lightdm

---
+++ usr/local/bin/lightdm.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# set intel gfx driver cfg file so Xorg doesn't freeze and launch lightdm
+
+readonly INTEL_CONF_FILE=/etc/X11/xorg.conf.d/20-intel.conf
+
+# if AMD and no intel graphics card, set intel driver to "modesetting"
+if lsmod | grep -E amdgpu && ! lsmod | grep -E i915; then
+    [[ -f "$INTEL_CONF_FILE" ]] &&
+        sed -i -e 's|Driver "intel"|Driver "modesetting"|g' "$INTEL_CONF_FILE"
+elif [[ -f "$INTEL_CONF_FILE" ]]; then # set intel driver to "intel"
+    sed -i -e 's|Driver "modesetting"|Driver "intel"|g' "$INTEL_CONF_FILE"
+fi
+
+$(which lightdm) # run lightdm
