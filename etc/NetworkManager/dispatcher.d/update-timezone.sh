e406dcefcfedf022130808415f19e7c1165689f3
mperrakis2 <169139627+mperrakis2@users.noreply.github.com>
2024-11-03 11:23:20 +0200

    updated with correct syntax

--- etc/NetworkManager/dispatcher.d/update-timezone.sh
+++ etc/NetworkManager/dispatcher.d/update-timezone.sh
@@ -4,8 +4,8 @@ iface=$1
 action=$2
 
 if [[ $iface != lo && $action == up ]]; then
-    tz=$(tzupdate -p 2>/dev/null)
-    if [[ -n $tz && -r /usr/share/zoneinfo/$tz && ! pgrep protonvpn-app > /dev/null ]]
+    tz=$(tzupdate -p 2> /dev/null)
+    if [[ $tz && -r /usr/share/zoneinfo/$tz ]] && ! pgrep protonvpn-app > /dev/null
     then
         timedatectl set-timezone $tz
     fi
