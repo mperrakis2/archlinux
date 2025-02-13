d85877b7c116996c67e4b504828d3be5a53f5836
mperrakis2 <169139627+mperrakis2@users.noreply.github.com>
2025-02-13 16:34:48 +0200

    improved regex

--- usr/sbin/cloneup/lib-functions.sh
+++ usr/sbin/cloneup/lib-functions.sh
@@ -927,8 +927,11 @@ system_sleep() {
         rsync_excludes=$(expr "$slp_entry" : "^.\+ $SEP \(.\+\)$")
 
         for target in "${TARGETS[@]}"; do
-            if [[ "$rsync_excludes" =~ "$target" ]]; then # add rsync filter
-                rsync_exclude="-f \"- $SLP_CFG_DIR/$target\" "
+            slp_entry="$SLP_CFG_DIR/$target"
+            if [[ "$rsync_excludes" =~ [[:space:]]+"$slp_entry"[[:space:]]+ ]]
+            then
+                 # add rsync filter
+                rsync_exclude="-f \"- $slp_entry\" "
                 rsync_filters["$SLP_CFG_MNT_DIR"]+="$rsync_exclude"
             fi
         done
