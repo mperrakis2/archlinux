d5af012453858590d4ecc122b397f3b123a8a744
mperrakis2 <169139627+mperrakis2@users.noreply.github.com>
2025-03-12 22:22:15 +0200

    bash.bashrc ----------- * removed unsafe aliases * fixed bug in pacman alias so that it does not automatically run under   sudo
    pacman_keyring.sh
    -----------------
    * fixed bug to not attempt to install package and update package DBs
      unless run with sudo
    * add functionality to echo 'sudo' if run under pacman alias and with
      sudo access

--- etc/bash.bashrc
+++ etc/bash.bashrc
@@ -5,10 +5,6 @@ alias l=ls
 alias la="ls -A"
 alias ll="ls -lah"
 
-alias cp="cp -f"
-alias mv="mv -f"
-alias rm="rm -f"
-
 alias grep="grep --color=auto"
 alias fgrep="grep -F --color=auto"
 alias egrep="grep -E --color=auto"
@@ -18,7 +14,7 @@ alias diff='diff --color=auto'
 alias sudo="sudo " # so that aliases can be used when calling sudo
 
 if groups "$(whoami)" | grep wheel &> /dev/null; then
-    alias pacman="sudo /usr/bin/pacman_keyring.sh && sudo pacman"
+    alias pacman='$(/usr/bin/pacman_keyring.sh pacman) $(which pacman)'
     alias pacaur="sudo /usr/bin/pacman_keyring.sh && pacaur -y"
     alias paru="sudo /usr/bin/pacman_keyring.sh && paru"
     export SUDO_EDITOR=mousepad
--- usr/bin/pacman_keyring.sh
+++ usr/bin/pacman_keyring.sh
@@ -1,18 +1,22 @@
 #!/usr/bin/bash
  
-# this script is called whenever pacman or paru are run; it's in aliases in
-# /etc/skel/.bashrc
+# this script is called whenever pacman, paru or pacaur are run; it's in aliases
+# in /etc/bash.bashrc
 
-pacman=$(which pacman)
 declare -i err=0
 
-if "$pacman" -Qu archlinux-keyring; then       # check update for keyring
-    "$pacman" --noconfirm -S archlinux-keyring # update keyring
-    let err=$?
-    if (( ! err )); then
-        "$pacman" -Fy                          # update package lists
-        let err=$?
+if (( $(id -u) == 0 )); then
+    pacman=$(which pacman)
+    if "$pacman" -Qu archlinux-keyring; then       # check update for keyring
+        "$pacman" --noconfirm -S archlinux-keyring # update keyring
+        ((err=$?))
+        if (( ! err )); then
+            "$pacman" -Fy                          # update package lists
+            ((err=$?))
+        fi
     fi
+
+    [[ $err -eq 0 && "$1" == pacman ]] && echo sudo
 fi
 
 exit $err
