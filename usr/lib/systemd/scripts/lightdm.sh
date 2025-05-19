4abfa137314811264bfd1cbfc2b141e04e0d3e84
mperrakis2 <169139627+mperrakis2@users.noreply.github.com>
2025-05-19 19:03:08 +0300

    set driveer to intel only if one GPU

--- usr/lib/systemd/scripts/lightdm.sh
+++ usr/lib/systemd/scripts/lightdm.sh
@@ -4,11 +4,17 @@
 
 readonly INTEL_CONF_FILE=/etc/X11/xorg.conf.d/20-intel.conf
 
+declare -i numGPU=0
+
+numGPU=$(lspci | grep -iE 'vga|3d|2d' | wc -l)
+
 # if AMD and no intel graphics card, set intel driver to "modesetting"
 if lsmod | grep -E amdgpu && ! lsmod | grep -E i915; then
     [[ -f "$INTEL_CONF_FILE" ]] &&
         sed -i -e 's|Driver "intel"|Driver "modesetting"|g' "$INTEL_CONF_FILE"
-elif [[ -f "$INTEL_CONF_FILE" ]]; then # set intel driver to "intel"
+
+# set intel driver to "intel"
+elif [[ -f "$INTEL_CONF_FILE" && "$numGPU" && "$numGPU" -eq 1 ]]; then
     sed -i -e 's|Driver "modesetting"|Driver "intel"|g' "$INTEL_CONF_FILE"
 fi
 
