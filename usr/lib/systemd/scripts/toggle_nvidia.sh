a889ae8738904ec0150dfa57742806a20f801347
mperrakis2 <169139627+mperrakis2@users.noreply.github.com>
2025-05-22 08:00:55 +0300

    override.conf & lightdm.sh -------------------------- removed files and moved their functionanlity to toggle_nvidia.*
    toggle_nvidia.service & toggle_nvidia.sh
    ----------------------------------------
    moved functionality from override.conf & lightdm.sh to these files

--- usr/lib/systemd/scripts/toggle_nvidia.sh
+++ usr/lib/systemd/scripts/toggle_nvidia.sh
@@ -15,3 +15,21 @@ else
         fi
     done
 fi
+
+# update cfg file for intel gfx driver so Xorg doesn't freeze
+
+readonly INTEL_CONF_FILE=/etc/X11/xorg.conf.d/20-intel.conf
+
+if [[ -f "$INTEL_CONF_FILE" ]]; then
+    declare -a GPUs=()
+
+    mapfile -t GPUs < <(lspci | grep -iE 'vga|3d|2d')
+
+    # set intel driver to "intel" if only one GPU
+    if [[ ${#GPUs[@]} -eq 1 && "${GPUs[0]}" =~ [Ii][Nn][Tt][Ee][Ll] ]]
+    then
+        sed -i -e 's|Driver[[:space:]]\+"modesetting"|Driver "intel"|g' "$INTEL_CONF_FILE"
+    else
+        sed -i -e 's|Driver[[:space:]]\+"intel"|Driver "modesetting"|g' "$INTEL_CONF_FILE"
+    fi
+fi
