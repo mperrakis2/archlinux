6ced75e75c00c9d97496544e6fd11b515039effe
mperrakis2 <169139627+mperrakis2@users.noreply.github.com>
2025-05-31 17:19:52 +0300

    envycontrol.service ------------------- run script under user privileges not sudo
    envycontrol.sh
    --------------
    * check if user has sudo access
    * replace paru with pamac
    * update comments

--- usr/lib/systemd/scripts/envycontrol.sh
+++ usr/lib/systemd/scripts/envycontrol.sh
@@ -3,20 +3,29 @@
 # enable/disable envycontrol (nvidia hybrid graphics) depending on
 # nvidia kernel module being loaded or not
 
-if paru -Qsq envycontrol &> /dev/null; then # nvidia hybrid package installed
-    if ! lsmod | grep -iwq nvidia; then     # PC with no nvidia card
+# check if user has sudo access
+if getent passwd | cut -d ':' -f 1 | xargs -n 1 sudo -l -U &> /dev/null; then
+    pm="sudo pamac" # sudo access to package manager (pm)
+    pkg="envycontrol"
 
-        # uninstall nvidia package and stop/disable its service
-        pamac remove --no-confirm envycontrol
+    # nvidia hybrid package installed
+    if [[ $($pm search --installed --quiet "$pkg") ]]; then
+        if ! lsmod | grep -iwq nvidia; then # no nvidia card
+
+            # uninstall nvidia package and stop/disable its service
+            $pm remove --no-confirm "$pkg"
+        fi
+    elif lsmod | grep -iwq nvidia; then # PC with nvidia card
+        script="sudo $pkg" # sudo access to script
+
+        # get display manager
+        dm=$(systemctl --property=Id show display-manager.service)
+        dm="${dm//*=}" # remove all up to and including '='
+        dm="${dm//.*}" # remove all from '.' and after
+
+        # install nvidia hybrid package and set it up
+        $pm install --no-confirm "$pkg" &&
+        $script -s hybrid --dm "$dm" --rtd3 0 &&
+        $script --cache-create
     fi
-elif lsmod | grep -iwq nvidia; then # PC with nvidia card
-    # get display manager
-    dm=$(systemctl --property=Id show display-manager.service)
-    dm="${dm//*=}" # remove all up to and including '='
-    dm="${dm//.*}" # remove all from '.' and after
-
-    # install nvidia hybrid package and set it up
-    pamac install --no-confirm envycontrol &&
-    envycontrol -s hybrid --dm "$dm" --rtd3 0 &&
-    envycontrol --cache-create
 fi
