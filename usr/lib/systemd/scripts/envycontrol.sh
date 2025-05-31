b4e1c9fc07c8cddd2efccb21c4b4173ba34ef068
mperrakis2 <169139627+mperrakis2@users.noreply.github.com>
2025-05-31 09:20:03 +0300

    switched from paru to pamac and removed "su" as it's running under root anyway

--- usr/lib/systemd/scripts/envycontrol.sh
+++ usr/lib/systemd/scripts/envycontrol.sh
@@ -7,7 +7,7 @@ if paru -Qsq envycontrol &> /dev/null; then # nvidia hybrid package installed
     if ! lsmod | grep -iwq nvidia; then     # PC with no nvidia card
 
         # uninstall nvidia package and stop/disable its service
-        paru --noconfirm -Rns envycontrol
+        pamac remove --no-confirm envycontrol
     fi
 elif lsmod | grep -iwq nvidia; then # PC with nvidia card
     # get display manager
@@ -15,10 +15,8 @@ elif lsmod | grep -iwq nvidia; then # PC with nvidia card
     dm="${dm//*=}" # remove all up to and including '='
     dm="${dm//.*}" # remove all from '.' and after
 
-    # install nvidia hybrid package
-    su -c "paru --noconfirm -Sq envycontrol" - "$(logname)" &&
-
-    # setup nvidia hybrid package
+    # install nvidia hybrid package and set it up
+    pamac install --no-confirm envycontrol &&
     envycontrol -s hybrid --dm "$dm" --rtd3 0 &&
     envycontrol --cache-create
 fi
